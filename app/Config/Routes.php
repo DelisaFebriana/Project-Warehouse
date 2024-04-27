078f0ca7241a914e7da3b2cbc681a32df341297e
DelisaFebriana <delisa260204@gmail.com>
2024-04-27 13:13:00 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
+$routes->get('/', 'HelloController::index');
